df587820c74f5aeba265cf01cd8ba96a86601a37
Eleonora <61467087+EleonoraR@users.noreply.github.com>
2023-05-27 21:33:33 -0400

    Create main.py
    with numbers- int float and math functions

---
+++ main.py
@@ -0,0 +1,16 @@
+#Section 3. Lesson 21.Numbers int float----saved as binary in memory--------
+
+print(type(2 + 3))
+print(type(2 - 3))
+print(type(2 * 5))
+print(type(2 / 4))
+
+print(2**3)
+print(2 // 4)
+print(5 % 4)
+#Section 3. Lesson 21.Numbers int float-------------------------------
+
+#Section 3. Lesson 22. Math Functions----------------------
+print(round(3.7))
+print(abs(-20))
+#Section 3. Lesson 22. Math Functions--------------------
