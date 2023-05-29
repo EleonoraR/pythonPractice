6da17ac58418a3c7dcf5f429ec0dca0c5abe5c6f
Eleonora <61467087+EleonoraR@users.noreply.github.com>
2023-05-28 20:36:56 -0400

    Update main.py
    Practice Dictionary Added

--- main.py
+++ main.py
@@ -14,3 +14,58 @@ print(5 % 4)
 print(round(3.7))
 print(abs(-20))
 #Section 3. Lesson 22. Math Functions--------------------
+
+#Section 3. Lesson 51. Dictionary---------
+dictionary = {'a': [1, 2, 3], 'b': 'hello', 'x': True}
+
+my_list = [{
+  'a': [1, 2, 3],
+  'b': 'hello',
+  'x': True
+}, {
+  'a': [4, 5, 6],
+  'b': 'bye',
+  'x': False
+}]
+print(my_list[0]['a'][2])
+print(dictionary['a'][1])
+#Section 3. Lesson 51. Dictionary---------
+
+#Section 3. Lesson 53. Dictionary Key---------
+dictionary_user = {
+  123: [1, 2, 3],
+  'greet': 'hello',
+  'is_Magic': True  #[100]:True ---doesnt work
+}
+
+print(dictionary_user[123])
+print(dictionary_user['greet'])
+
+#Section 3. Lesson 53. Dictionary Key---------
+
+#Section 3. Lesson 54. Dictionary Methods---------
+user = {'basket': [1, 2, 3], 'greet': 'hello', 'age': 20}
+print(user.get('age', 55))
+
+user2 = dict(name="JohnJohn")
+print(user2)
+
+#Section 3. Lesson 54. Dictionary Methods---------
+
+#Section 3. Lesson 55. Dictionary Methods 2---------
+print('basket' in user.keys())
+print('hello' in user.values())
+print(user.items())
+user1 = user.copy()
+
+user.clear()
+print(user)
+print(user1)
+print(user1.pop('age'))
+print(user1)
+print(user1.popitem())
+print(user1)
+print(user1.update({'age': 55}))
+print(user1)
+#Section 3. Lesson 55. Dictionary Methods 2---------
+
