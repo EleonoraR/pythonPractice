4c220d3cbfc8c5bbe91cf5633c13ced16e15edeb
Eleonora <61467087+EleonoraR@users.noreply.github.com>
2023-06-21 19:21:05 -0400

    Create stringSplit.py

---
+++ stringSplit.py
@@ -0,0 +1,9 @@
+st_with_t = 'Print only the words that with e in this sentence'
+for word in st_with_t.split():
+    if word[0] == 't':
+        print(word)
+        
+        
+# the
+# that
+# this
