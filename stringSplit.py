b14615934acfbe5b9cfd3be4e7ba5a6c4a1cbed8
Eleonora <61467087+EleonoraR@users.noreply.github.com>
2023-06-26 18:37:35 -0400

    Update stringSplit.py
    split()

--- stringSplit.py
+++ stringSplit.py
@@ -1,9 +1,15 @@
-st_with_t = 'Print only the words that with e in this sentence'
+st_with_t = 'Print only the words that with t in this sentence'
 for word in st_with_t.split():
     if word[0] == 't':
         print(word)
         
-        
 # the
 # that
 # this
+
+new_string = 'Hello World. You are beautiful'
+split_new_string = new_string.split()
+print(split_new_string)
+
+#['Hello', 'World.', 'You', 'are', 'beautiful']
+
