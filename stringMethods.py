1ef5d51d4d9f880a61c50019006a3bf277136487
Eleonora <61467087+EleonoraR@users.noreply.github.com>
2023-07-07 21:47:34 -0400

    Create stringMethods.py

---
+++ stringMethods.py
@@ -0,0 +1,10 @@
+sentence = 'How was your day?'
+print(sentence.swapcase())
+print(sentence.capitalize())
+print(sentence.upper())
+print(sentence.lower())
+print(sentence.title())
+
+print(sentence.title().swapcase())
+
+
