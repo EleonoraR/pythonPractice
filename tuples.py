758bf5816e8c226e8324e03b5ba6f96384e21777
Eleonora <61467087+EleonoraR@users.noreply.github.com>
2023-07-09 20:57:18 -0400

    Create tuples.py

---
+++ tuples.py
@@ -0,0 +1,16 @@
+my_tuple = ("h","e","l","l","o")
+print(my_tuple)
+type(my_tuple)
+tuple_word = "world"
+my_tuple2 = tuple(tuple_word)
+print(my_tuple2)
+new_list = [10,20,"crazy","world"]
+type(new_list)
+new_tuple = tuple(new_list)
+print(new_tuple)
+tuple_var = "hello", 3,4,5,"music","science"
+print(tuple_var)
+
+my_t = ("time",)
+type(my_t)
+
