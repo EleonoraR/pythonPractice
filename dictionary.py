6ee846caefaa2176c72eb1366189365d3689fdd9
Eleonora <61467087+EleonoraR@users.noreply.github.com>
2023-07-10 21:00:27 -0400

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,33 @@
+vehicle = {"car": "lamborgini", "airplane":"F16"}
+print(vehicle, type(vehicle), sep="\n")
+vehicle2 = dict(car = "ferrari", airplane = "airbus")
+print(vehicle2, type(vehicle2), sep = "\n")
+
+capital = {"USA": "washington",
+          "Austria": "vienna",
+          "Germany": "berlin",
+          "UK": "london"}
+
+print(capital["Germany"])
+
+school_notes = {"school1": {"CLASS A": 75,
+                            "CLASS B": 80,
+                            "CLASS C": 70},
+                
+                "school2": {"CLASS A": 65,
+                            "CLASS B": 70,
+                            "CLASS C": 75},
+                
+                "school3": {"CLASS A": 90,
+                            "CLASS B": 85,
+                            "CLASS C": 80}
+               }
+print(school_notes["school2"])
+print(school_notes["school3"]["CLASS C"])
+
+capital["Ukraine"] = "Kuiv"
+print(capital)
+
+#pop method
+capital.pop("Germany")
+print(capital)
